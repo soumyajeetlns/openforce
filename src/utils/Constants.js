5102bf33957bfb84cabb8aa71d5a0581cd456dfb
Pritam Naskar <pritam.lnsel@gmail.com>
2019-06-20 15:31:49 +0530

    changes for fonts

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -4,6 +4,7 @@ import {ProjectId} from "./FirebaseConfigurator";
 export const ENVIRONMENT = process.env.NODE_ENV;
 export const GOOGLE_MAPS_API_KEY = "AIzaSyB8zsVhllcu62bJaxjdLY_UFcGLkf-bDCo";
 export const HOME = "/"
+
 export const SIGNIN_EMPLOYER = "/employer/signin"
 export const SIGNUP_EMPLOYER = "/employer/signup"
 export const SIGNOUT_EMPLOYER = "/employer/signout"
